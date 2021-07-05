fc5c1c9ea6e927b0384d6b3215c4981a26fe5a59
Roman <romanshuryhins2rent@gmail.com>
2021-07-05 20:10:45 +0300

    fix server index

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.use('/api', bannerRouter);
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-	app.use(express.static( '../client/build'));
+	app.use(express.static( path.join(__dirname, '../client/build')));
 
 	app.get('*', (req, res) =>  {
 		res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
